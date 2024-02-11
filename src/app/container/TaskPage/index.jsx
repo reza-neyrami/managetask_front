473544af11c4720884267c96ac98c442bda418b5
neyrami <neyrami.65@gmail.com>
2024-02-11 19:49:20 +0330

    create add page and page size

--- src/app/container/TaskPage/index.jsx
+++ src/app/container/TaskPage/index.jsx
@@ -1,8 +1,27 @@
 import DashboardPage from "./../DashboardPage/index.jsx";
 import { Grid } from "@mui/material";
+import PropTypes from "prop-types";
 import ListPaginate from "./../../component/ListPaginate/index.jsx";
 import { columns } from "./Cell.jsx";
+import { useEffect } from 'react';
 export default function TaskPage({ category }) {
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  // const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    // getCategorys({ page, size: pageSize });
+  }, [page, pageSize]);
+
+  function handlePageChange(p, s) {
+    setPage(p);
+    setPageSize(s);
+  }
+  function handlePageSizeChange(s) {
+    setPageSize(s);
+  }
+
   return (
     <DashboardPage>
       <div>
@@ -29,7 +48,6 @@ export default function TaskPage({ category }) {
   );
 }
 TaskPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
   getCategorys: PropTypes.func.isRequired,
   category: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
 };
